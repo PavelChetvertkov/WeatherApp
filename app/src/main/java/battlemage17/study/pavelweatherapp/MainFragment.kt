0bbfd6081a2175643807ba35a34415f7eb597f57
battlemage17 <pavelchetvertkov@mail.ru>
2023-01-16 22:52:21 +0300

    commit 16.01.23 22:52

--- app/src/main/java/battlemage17/study/pavelweatherapp/MainFragment.kt
+++ app/src/main/java/battlemage17/study/pavelweatherapp/MainFragment.kt
@@ -2,60 +2,26 @@ package battlemage17.study.pavelweatherapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import battlemage17.study.pavelweatherapp.databinding.FragmentMainBinding
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
-import org.json.JSONObject
 import java.util.*
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
     private val viewModelWeather: WeatherViewModel by activityViewModels()
-    private val selectedLanguage = Locale.getDefault().language.toString()
-
-    private fun translateCondition() {
-        val urlCondition =
-            "https://www.weatherapi.com/docs/conditions.json"
-        val queueCondition = Volley.newRequestQueue(requireActivity())
-        val stringRequestCondition = StringRequest(
-            Request.Method.GET,
-            urlCondition,
-            { response ->
-                //val obj =
-                    JSONObject(response)
-
-                //val textWeather = obj.getJSONObject("languages").getString("lang_iso")
-                //if (selectedLanguage == textWeather)
-
-                Toast.makeText(requireActivity(), "Response: $response", Toast.LENGTH_SHORT).show()
-                Log.d("MyLog", "Response: $response")
-            },
-            {
-                Toast.makeText(requireActivity(), "Error: $it", Toast.LENGTH_SHORT).show()
-                Log.d("MyLog", "Error: $it")
-            }
-        )
-        queueCondition.add(stringRequestCondition)
-    }
-
-    private fun translateConditionRetroFit() {
-
-    }
+    private var translateCondition = ""
 
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModelWeather.message.observe(viewLifecycleOwner) {
+            translateCondition = it.textWeather
             binding.tvCurrentWeather.text =
                 "${getString(R.string.weather_in_the_selected_lovation)}:\n" +
                         "${it.textWeather}\n" +
@@ -64,8 +30,6 @@ class MainFragment : Fragment() {
                         "${getString(R.string.wind_speed)}: ${it.windKph} ${getString(R.string.kmh)}\n" +
                         "${getString(R.string.gust)}: ${it.gustKph} ${getString(R.string.kmh)}\n" +
                         "${getString(R.string.last_updated)}: ${it.lastUpdated}"
-
-            //translateCondition()
 
             Glide.with(this).clear(binding.ivIconWeather)
 
@@ -79,6 +43,7 @@ class MainFragment : Fragment() {
 
         binding.bGetWeather.setOnClickListener {
             viewModelWeather.getResult(binding.svEnterPlace.query.toString())
+            viewModelWeather.getTranslationCondition(translateCondition)
         }
     }
 
--- app/src/main/java/battlemage17/study/pavelweatherapp/WeatherViewModel.kt
+++ app/src/main/java/battlemage17/study/pavelweatherapp/WeatherViewModel.kt
@@ -6,9 +6,11 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONObject
+import java.util.*
 
 private const val API_KEY = "b8690996b95c4beb877105434221212"
 
@@ -16,42 +18,48 @@ class WeatherViewModel(application: Application) : AndroidViewModel(application)
     private val _message = MutableLiveData<DailyWeather>()
     val message: LiveData<DailyWeather> get() = _message
 
+    private val selectedLanguage = Locale.getDefault().language.toString()
+
     //fun getResult(place: String): DailyWeather {
     fun getResult(place: String) {
         val url =
             "https://api.weatherapi.com/v1/current.json?key=$API_KEY&q=$place&days=1&aqi=no&alerts=no"
         val queue = Volley.newRequestQueue(getApplication<Application?>().applicationContext)
-        val stringRequest = StringRequest(
-            Request.Method.GET,
-            url,
-            { response ->
-                val obj = JSONObject(response)
-                val iconWeather =
-                    obj.getJSONObject("current").getJSONObject("condition").getString("icon")
-                val textWeather =
-                    obj.getJSONObject("current").getJSONObject("condition").getString("text")
-                val lastUpdated = obj.getJSONObject("current").getString("last_updated")
-                val tempC = obj.getJSONObject("current").getString("temp_c")
-                val windKph = obj.getJSONObject("current").getString("wind_kph")
-                val feelsLikeC = obj.getJSONObject("current").getString("feelslike_c")
-                val gustKph = obj.getJSONObject("current").getString("gust_kph")
+        val stringRequest = StringRequest(Request.Method.GET, url, { response ->
+            val obj = JSONObject(response)
+            val iconWeather =
+                obj.getJSONObject("current").getJSONObject("condition").getString("icon")
+            val textWeather =
+                obj.getJSONObject("current").getJSONObject("condition").getString("text")
+            val lastUpdated = obj.getJSONObject("current").getString("last_updated")
+            val tempC = obj.getJSONObject("current").getString("temp_c")
+            val windKph = obj.getJSONObject("current").getString("wind_kph")
+            val feelsLikeC = obj.getJSONObject("current").getString("feelslike_c")
+            val gustKph = obj.getJSONObject("current").getString("gust_kph")
 
-                _message.value = DailyWeather(
-                    iconWeather,
-                    textWeather,
-                    lastUpdated,
-                    tempC,
-                    windKph,
-                    feelsLikeC,
-                    gustKph
-                )
-                Log.d("MyLog", "Response: $response")
-            },
-            {
-                Log.d("MyLog", "Error: $it")
-            }
-        )
+            _message.value = DailyWeather(
+                iconWeather, textWeather, lastUpdated, tempC, windKph, feelsLikeC, gustKph
+            )
+            Log.d("MyLog", "Response: $response")
+        }, {
+            Log.d("MyLog", "Error: $it")
+        })
         queue.add(stringRequest)
         //return DailyWeather (iconWeather, textWeather, lastUpdated, tempC, windKph, feelsLikeC, gustKph)
+    }
+
+    fun getTranslationCondition(translateCondition: String) {
+        val url = "https://www.weatherapi.com/docs/conditions.json"
+        val queue = Volley.newRequestQueue(getApplication<Application?>().applicationContext)
+        val jsonArrayRequest = JsonArrayRequest(Request.Method.GET, url, null,
+            { response ->
+                //read an JSON array
+                Log.d("MyLog", "Response: $response")
+            },
+            { error ->
+                Log.d("MyLog", "Error: $error")
+            }
+        )
+        queue.add(jsonArrayRequest)
     }
 }
